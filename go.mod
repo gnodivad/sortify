abda2ae909ec9afbdef600803bd24f425852355d
DavidODW <dodwei.dev@gmail.com>
2019-11-29 21:22:24 +0800

    Add client credentials for spotify

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gnodivad/spot-spotify
+
+go 1.13
+
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/zmb3/spotify v0.0.0-20191028153142-869e03dbd8b0
+	golang.org/x/oauth2 v0.0.0-20191122200657-5d9234df094c
+)
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/zmb3/spotify"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config := &clientcredentials.Config{
+		ClientID:     os.Getenv("SPOTIFY_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
+		TokenURL:     spotify.TokenURL,
+	}
+	token, err := config.Token(context.Background())
+	if err != nil {
+		log.Fatalf("couldn't get token: %v", err)
+	}
+
+	client := spotify.Authenticator{}.NewClient(token)
+}
