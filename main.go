f98fb7b0fec0508a560da6bcd2ec696855dd36d8
DavidODW <dodwei.dev@gmail.com>
2019-12-04 19:11:46 +0800

    Get user by using oauth

--- main.go
+++ main.go
@@ -1,39 +1,60 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"os"
+	"net/http"
 
 	"github.com/joho/godotenv"
 	"github.com/zmb3/spotify"
-	"golang.org/x/oauth2/clientcredentials"
+)
+
+// redirectURI is the OAuth redirect URI for the application.
+// You must register an application at Spotify's developer portal
+// and enter this value.
+const redirectURI = "http://localhost:8080/callback"
+
+var (
+	err   = godotenv.Load()
+	auth  = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// first start an HTTP server
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go http.ListenAndServe(":8080", nil)
 
-	config := &clientcredentials.Config{
-		ClientID:     os.Getenv("SPOTIFY_ID"),
-		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
-		TokenURL:     spotify.TokenURL,
-	}
-	token, err := config.Token(context.Background())
-	if err != nil {
-		log.Fatalf("couldn't get token: %v", err)
-	}
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
 
-	client := spotify.Authenticator{}.NewClient(token)
-	page, err := client.GetPlaylistsForUser("dodwei")
-	if err != nil {
-		log.Fatalf("couldn't get features playlists: %v", err)
-	}
+	// wait for auth to complete
+	client := <-ch
 
-	for _, playlist := range page.Playlists {
-		fmt.Println(playlist.Name, " ", playlist.ID)
+	// use the client to make calls that require authorization
+	user, err := client.CurrentUser()
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Println("You are logged in as:", user.ID)
+}
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+	// use the token to get an authenticated client
+	client := auth.NewClient(tok)
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- &client
 }
